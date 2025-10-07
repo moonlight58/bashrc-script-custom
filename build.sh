1593865c4bacd7d76945016ade538bfed529f66e
moonlight58 <gaelrothlin@gmail.com>
2025-10-07 15:35:02 +0200

    prod test

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 rm -f ../bashrc-script-custom_*.deb ../bashrc-script-custom_*.buildinfo ../bashrc-script-custom_*.changes
 
 # Générer la version
-VERSION="1.0.2"  # ou 1.0-$(date +%Y%m%d%H%M)
+VERSION="1.0.3"  # ou 1.0-$(date +%Y%m%d%H%M)
 
 # Build le package
 dpkg-deb --build debian/bashrc-script-custom ../bashrc-script-custom_${VERSION}_all.deb
