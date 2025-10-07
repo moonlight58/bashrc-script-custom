55c8cd1d2bcd3bcac641a4df6f818a18d5c07432
moonlight58 <gaelrothlin@gmail.com>
2025-10-07 17:32:47 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ rm -f ../bashrc-script-custom_*.deb ../bashrc-script-custom_*.buildinfo ../bashr
 # Générer la version
 VERSION="1.0.3"  # ou 1.0-$(date +%Y%m%d%H%M)
 
+# Clean les packages précédents
+/home/userdepinfo/Documents/github/moon-apt-repo/clean_pkg
+
 # Build le package
 dpkg-deb --build debian/bashrc-script-custom ../bashrc-script-custom_${VERSION}_all.deb
+cp ../bashrc-script-custom_${VERSION}_all.deb ../moon-apt-repo/pool/main/b/
 
 echo "✓ Package créé : ../bashrc-script-custom_${VERSION}_all.deb"
